0e3e774d60d15a1e54015a9568dffe93f4373837
mincheol <mincheol@vt.edu>
2018-04-29 21:06:46 -0400

    All implementation should be converged on invyswell.h, and each transaction should be implemented on each file (e.q., BFHW.c)

--- invyswell.h
+++ invyswell.h
@@ -1,1 +1,9 @@
 #include "rtm.h"
+#include "BitFilter.h"
+
+/* BFHW functions */
+void BFHW_tx_begin(void);
+void BFHW_tx_read(void);
+void BFHW_tx_write(void);
+void BFHW_tx_end(void);
+void BFHW_tx_post_commit(void);
